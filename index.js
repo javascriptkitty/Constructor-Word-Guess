896f10ea81dac02cc687e864707a498165dadd84
Cristina Alekseeva <cristina.alekseeva@gmail.com>
2019-06-23 21:34:16 -0700

    added reset function

--- index.js
+++ index.js
@@ -1,18 +1,89 @@
 var Word = require("./word.js");
 var inquirer = require("inquirer");
 
-var guessesLeft = 12;
-var randomWord = "word";
-var newWord = new Word(randomWord);
+var categories = {
+  animals: [
+    "antelope"
+    // "kangaroo",
+    // "rooster",
+    // "octopus",
+    // "ostrich",
+    // "squirrel",
+    // "capybara",
+    // "hedgehog"
+  ],
+  food: [
+    "cheesecake",
+    "gingerbread",
+    "dumplings",
+    "marshmellow",
+    "sandwich",
+    "spaghetti",
+    "fondue",
+    "guacamole"
+  ],
+  at_the_beach: [
+    "surfboard",
+    "swimsuit",
+    "sunglasses",
+    "sunscreen",
+    "sandcastle",
+    "seagull",
+    "seashells",
+    "umbrella"
+  ],
+  vehicles: [
+    "airplane",
+    "motorcycle",
+    "subway",
+    "carriage",
+    "skateboard",
+    "helicopter",
+    "scooter",
+    "canoe"
+  ]
+};
 
+function getRandom(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+var randomWord;
+var newWord;
 var count = 0;
+var guessesLeft;
+var randomWord;
+
+function reset() {
+  count = 0;
+  guessesLeft = 12;
+  randomWord = "word";
+
+  inquirer
+    .prompt({
+      type: "list",
+      name: "usersChoice",
+      message: "Choose a category!",
+      choices: ["animals", "food", "at_the_beach", "vehicles"]
+    })
+    .then(function(getCategory) {
+      var usersChoice = categories[getCategory.usersChoice];
+      randomWord = getRandom(usersChoice);
+      newWord = new Word(randomWord);
+    })
+    .then(askUser);
+}
+
 var askUser = function() {
-  if (count < 12) {
-    inquirer
+  if (guessesLeft > 0) {
+    return inquirer
       .prompt({
         type: "input",
         name: "usersGuess",
-        message: "Guess a letter!"
+        message: "Guess a letter!",
+        validate: function validateInput(name) {
+          return name !== "";
+        }
       })
       .then(function(guess) {
         if (randomWord.indexOf(guess.usersGuess) != -1) {
@@ -32,11 +103,11 @@ var askUser = function() {
           console.log(guessesLeft + " guesses remaining!");
         }
         count++;
-        askUser();
+        return askUser();
       });
   } else {
     console.log("YOU LOST! THE WORD IS " + randomWord);
   }
 };
 
-askUser();
+reset();
